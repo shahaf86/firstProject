19bc619a16690106c35393c064a5db5ec3cf46ed
shahaf86 <shahaf87@gmail.com>
2016-04-06 14:11:30 +0300

    Update note.js

--- js/note.js
+++ js/note.js
@@ -4,11 +4,13 @@ var inputDate;
 var getInputTitle;
 var getInputContent;
 var getInputDate;
+var arrayPosition;
 
-function note(title, content, date){
+function note(title, content, date, arrayPosition){
 	this.inputTitle = inputTitle;
 	this.inputContent = inputContent;
 	this.inputDate = inputDate;
+	this.arrayPosition = arrayPosition;
 	this.getInputTitle = function getInputTitle(){
 		return title;
 	}
